602c9b29d8f4c527aad342d22d04114434f8c8c4
muna <munawiki@gmail.com>
2023-05-15 17:00:03 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <TypographyTitle class="text-center">Hello World</TypographyTitle>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { TypographyTitle } from "ant-design-vue";
+</script>
+
+<style scoped></style>
