56b0606a732c2a3a9c03afbb4971d2a86414fc5c
muna <munawiki@gmail.com>
2023-05-15 17:14:02 +0900

    Refactor App.vue with Ant Design TypographyTitle

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
-    <TypographyTitle class="text-center">Hello World</TypographyTitle>
+    <a-typography-title class="hello-world">Ant Design Vue</a-typography-title>
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
 import { TypographyTitle } from "ant-design-vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {
+    "a-typography-title": TypographyTitle,
+  },
+});
 </script>
 
 <style scoped></style>
